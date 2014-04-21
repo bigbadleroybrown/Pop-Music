d6652791b5d09bb94ae1508de18b2ecd9dc1a430
Eugene Watson <eugenewats@gmail.com>
2014-04-21 15:13:41 -0400

    adding in datastore class

---
+++ musicVisualizedV.2/DataStore.h
@@ -0,0 +1,13 @@
+//
+//  DataStore.h
+//  musicVisualizedV.2
+//
+//  Created by Eugene Watson on 4/21/14.
+//  Copyright (c) 2014 Eugene Watson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataStore : NSObject
+
+@end
---
+++ musicVisualizedV.2/DataStore.m
@@ -0,0 +1,13 @@
+//
+//  DataStore.m
+//  musicVisualizedV.2
+//
+//  Created by Eugene Watson on 4/21/14.
+//  Copyright (c) 2014 Eugene Watson. All rights reserved.
+//
+
+#import "DataStore.h"
+
+@implementation DataStore
+
+@end
